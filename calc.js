0a52294ef2d1f01eb907a5fe8d319da04c7d97e6
Mubashira <mubisaifu2017@gmail.com>
2021-11-25 13:05:10 +0530

    initial

---
+++ calc.js
@@ -0,0 +1,46 @@
+var displayValue=(num)=> result.value+=num
+
+var clearBox=()=> result.value=""
+
+var evaluateExpression=()=> result.value=eval(result.value)
+
+var backSpace=()=>result.value=result.value.slice(0,-1)
+
+
+
+
+
+
+
+// function displayValue(num){
+//     // let res=document.querySelector("#result")//textbox accessed
+//     // res.value +=num;//to concatenate clicked values
+//     ///another method by directly calling id name.exclusively for id .not applicable for class
+//     result.value+=num //content s
+// }
+
+// function clearBox(){
+//     //document.querySelector("#result").value=""
+//     result.value=""
+// }
+
+// function evaluateExpression(){
+//     // let res=document.querySelector("#result")//textbox
+//     // let cur_value=res.value//value inside textbox
+//     // let result=eval(cur_value) //evaluate function called on content inside textbox
+//     // res.value=result//set textbox value as evaluated value
+
+//     result.value=eval(result.value)
+// }
+
+// // function backSpace(){
+//     let res=document.querySelector("#result")
+//     let cur_value=res.value
+//     res.value=cur_value.slice(0,-1);//to delete last content entered inside textbox
+// }
+
+
+
+
+
+
